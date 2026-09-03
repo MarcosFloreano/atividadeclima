58095410f1674c04b6f28f0fce81a157641826e2
MarcosFloreano <marangoflor@gmail.com>
2026-09-03 20:25:49 -0300

    Add files via upload

---
+++ atividadeclima/js/index.js
@@ -0,0 +1,71 @@
+let campoCidade = document.querySelector("#cidade");
+let elementoMensagem = document.querySelector("#mensagem");
+let elementoCidades = document.querySelector("#cidades")
+let elementoPrevisao = document.querySelector("#previsao")
+
+campoCidade.addEventListener("keydown", function (evento) {
+  if (evento.key == "Enter") {
+
+    buscarCidades();
+  }
+});
+
+async function buscarCidades() {
+  let nome = campoCidade.value;
+  elementoCidades.innerHTML = ""; 
+  elementoMensagem.textContent = "Buscando...";
+
+  let resposta = await fetch(
+    `https://brasilapi.com.br/api/cptec/v1/cidade/${nome}`,
+  );
+  let dados = await resposta.json();
+
+
+  if (resposta.ok) {
+    for (let i = 0; i < dados.length; i++) {
+      let elementoCidade = document.createElement("p");
+      elementoCidade.textContent = `${dados[i].nome} -${dados[i].estado}`;
+      elementoCidade.classList.add("cidade");
+      elementoCidade.addEventListener("click", function () {
+        buscarPrevisao(dados[i].id);
+      });
+      elementoCidades.appendChild(elementoCidade);
+    }
+
+    elementoMensagem.textContent = "";
+  } else {
+    elementoMensagem.textContent = dados.message;
+  }
+}
+
+async function buscarPrevisao(id) {
+  elementoPrevisao.textContent = "Buscando...";
+
+  let resposta = await fetch(
+    `https://brasilapi.com.br/api/cptec/v1/clima/previsao/${id}`,
+  );
+
+  let dados = await resposta.json();
+
+  if (resposta.ok) {
+    elementoPrevisao.innerHTML = `
+    <h2>${dados.cidade} ${dados.estado}</h2>
+    <div class="dia">
+    <P>Data: ${formatarData(dados.clima[0].data)} </p>
+    <P>Condição: ${dados.clima[0].condicao_desc} </p>
+    <P>Termperatura minima: ${dados.clima[0].min} °C </p>
+    <P>Termperatura máxima: ${dados.clima[0].max} °C </p>
+    <P>Índice UV: ${dados.clima[0].indice_uv} </p> 
+    </div>
+    `;
+    console.log(dados)
+  } else {
+    elementoPrevisao.textContent = dados.message;
+  }
+  console.log(dados);
+}
+
+function formatarData(data) {
+  let partes = data.split("-");
+  return `${partes[2]}/${partes[1]}/${partes[0]}`
+}
